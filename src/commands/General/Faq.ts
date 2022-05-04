a3f1df9ecc0188c3e89a200e926d0b41f3a6eb16
akumasugi <102664326+akumasugi@users.noreply.github.com>
2022-05-04 15:08:39 +0530

    Update Faq.ts

--- src/commands/General/Faq.ts
+++ src/commands/General/Faq.ts
@@ -17,7 +17,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            'https://telegra.ph/file/e8bf64fe80a97b96b7ce4.mp4'
+            'https://telegra.ph/file/f6fe1a2927f74c4bf8a35.mp4'
         ]
         let rin = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: rin }, MessageType.video, {quoted:M.WAMessage,
